55380fd7178289d9ea7fccc5ed449741d1dd62ea
AngusWu <anguswu@simbo.com.tw>
2018-06-23 09:35:21 +0800

    implement navbar

---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="navbar">
+    <div class="container">
+      <div class="navbar-brand">Shoptime</div>
+      <ul class="navbar-item">
+        <li class="navbar-item-content"><a href="#" class="active">HOME</a></li>
+        <li class="navbar-item-content"><a href="#">ORDERS</a></li>
+        <li class="navbar-item-content"><a href="#">PRODUCT</a></li>
+      </ul>
+      <div class="navbar-user">ADMIN</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+}
+</script>
