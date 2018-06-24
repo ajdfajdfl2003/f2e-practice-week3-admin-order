835eeba970e2cd2db175e42d0ed32b9efd81bcbe
AngusWu <anguswu@simbo.com.tw>
2018-06-24 20:34:07 +0800

    finish total and activity in Home

---
+++ src/common/LineChart.vue
@@ -0,0 +1,70 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  props: ['data', 'options'],
+  data () {
+    return {
+      chartOptions: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: true
+            }
+          }],
+          xAxes: [ {
+            gridLines: {
+              display: false
+            }
+          }]
+        },
+        legend: {
+          display: false
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
+  mounted () {
+    let vm = this
+
+    this.renderChart({
+      labels: ['6 JUN', '7 JUN', '8 JUN', '9 JUN', '10 JUN', '11 JUN', '12 JUN', '13 JUN'],
+      datasets: [
+        {
+          label: 'Revenue',
+          borderColor: '#7ED321',
+          pointBackgroundColor: '#7ED321',
+          borderWidth: 3,
+          pointBorderColor: '#7ED321',
+          backgroundColor: 'transparent',
+          data: [8000, 6000, 5600, 7200, 7800, 6000, 5500, 4877]
+        },
+        {
+          label: 'Cost',
+          borderColor: '#D0021B',
+          pointBackgroundColor: '#D0021B',
+          pointBorderColor: '#D0021B',
+          borderWidth: 3,
+          backgroundColor: 'transparent',
+          data: [6001, 5500, 6141, 5748, 5784, 6780, 6001, 5487]
+        },
+        {
+          label: 'Net Income',
+          borderColor: '#4A90E2',
+          pointBackgroundColor: '#4A90E2',
+          pointBorderColor: '#4A90E2',
+          borderWidth: 3,
+          backgroundColor: 'transparent',
+          data: [1001, 1200, 3320, 3587, 2358, 1567, 2451, 2368]
+        }
+      ]
+    }, vm.chartOptions)
+  }
+}
+</script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,30 +8,70 @@
         </div>
       </div>
       <div class="row">
-        <div class="card card-total"></div>
+        <div class="card card-total">
+          <div class="title">
+            <i class="fas fa-hand-holding-usd"></i>
+            <span>TOTAL REVENUE</span>
+          </div>
+          <span class="num green">$54,540</span>
+        </div>
         <div class="space"></div>
-        <div class="card card-total"></div>
+        <div class="card card-total">
+          <div class="title">
+            <i class="fas fa-boxes"></i>
+            <span>TOTAL COST</span>
+          </div>
+          <span class="num red">$12,660</span>
+        </div>
         <div class="space"></div>
-        <div class="card card-total"></div>
+        <div class="card card-total">
+          <div class="title">
+            <i class="fas fa-money-bill-wave"></i>
+            <span>NET INCOME</span>
+          </div>
+          <span class="num blue">$41,880</span>
+        </div>
       </div>
       <div class="row">
-        <div class="card card-activity"></div>
+        <div class="card card-activity">
+          <div class="h3-title">
+            <h3>Activity</h3>
+          </div>
+          <div class="chart">
+            <activity-line
+              :height="300"></activity-line>
+          </div>
+        </div>
       </div>
       <div class="row">
-        <div class="card card-two-column"></div>
+        <div class="card card-two-column">
+          <div class="h3-title">
+            <h3>Transaction Website</h3>
+          </div>
+          <div class="chart-zone"></div>
+        </div>
         <div class="space"></div>
-        <div class="card card-two-column"></div>
+        <div class="card card-two-column">
+          <div class="h3-title">
+            <h3>Latest Orders</h3>
+          </div>
+          <div class=""></div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import ActivityLine from '@/common/LineChart'
+
 export default {
   name: 'Home',
   data () {
-    return {
-    }
+    return {}
+  },
+  components: {
+    ActivityLine
   }
 }
 </script>
